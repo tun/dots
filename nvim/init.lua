f57b594d2f66d0f05acee3fd2f0d23e2eb92cd61
Ricardo Tun <me@ricardotun.mx>
2021-08-10 13:24:05 -0500

    add initial neovim config file

---
+++ nvim/init.lua
@@ -0,0 +1,52 @@
+--[[
+  Plugins
+]]--
+
+local fn = vim.fn
+
+local paq_install_path = fn.stdpath('data')..'/site/pack/paqs/opt/paq-nvim'
+local paq_plugins_path = fn.stdpath('data')..'/site/pack/paqs/start'
+
+if fn.empty(fn.glob(paq_install_path)) > 0 then
+  vim.api.nvim_command('!git clone https://github.com/savq/paq-nvim.git '..paq_install_path)
+end
+
+vim.cmd 'packadd paq-nvim'         -- Load package
+local paq = require'paq-nvim'.paq  -- Import module and bind `paq` function
+paq{'savq/paq-nvim', opt=true}     -- Let Paq manage itself
+
+paq 'junegunn/fzf'
+paq 'junegunn/fzf.vim'
+paq 'jiangmiao/auto-pairs' 
+paq 'mattn/emmet-vim'
+paq 'SirVer/ultisnips'
+paq 'honza/vim-snippets'
+paq 'tpope/vim-surround'
+-- Language support
+paq 'sheerun/vim-polyglot'
+paq 'slashmili/alchemist.vim'
+paq 'tpope/vim-endwise'
+paq 'dense-analysis/ale'
+-- Git support
+paq 'tpope/vim-fugitive'
+paq 'mhinz/vim-signify'
+-- Colorschemes
+paq 'ghifarit53/tokyonight-vim'
+paq 'pineapplegiant/spaceduck'
+
+if fn.empty(fn.glob(paq_plugins_path)) > 0 then
+  vim.cmd 'PaqInstall'
+end
+
+vim.o.background = "dark"
+vim.o.termguicolors = true
+vim.g['tokyonight_style'] = 'night'
+vim.g['tokyonight_enable_italic'] = 1
+vim.g['tokyonight_transparent_background'] = 0
+
+vim.cmd("silent! colorscheme tokyonight")
+
+vim.cmd('autocmd BufWritePost $MYVIMRC luafile $MYVIMRC')
+vim.cmd('autocmd VimResized * :wincmd =')
+
+vim.opt.runtimepath:append("~/.vim")
