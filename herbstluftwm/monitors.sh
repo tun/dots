8cfcad78de4934cbc4ffa52a2976e6f9373eb317
Ricardo Tun <me@ricardotun.mx>
2021-09-01 01:34:24 -0500

    improves herbstluftwm config

--- herbstluftwm/monitors.sh
+++ herbstluftwm/monitors.sh
@@ -7,11 +7,11 @@ for monitor in $(xrandr --listmonitors | grep -v Monitors | wc -l) ; do
     case $monitor in
     	1)
 			hc set_monitors 1920x1080+1280+0+0
-    		hc pad 0 20 0 0 0
+    		hc pad 0 0 0 0 0
     		;;
 
     	2)
-			hc spawn xrandr --auto --output HDMI1 --mode 1920x1080 --left-of eDP1 
+			hc spawn xrandr --auto --output HDMI1 --mode 1920x1080 --left-of eDP1
 			hc set_monitors 1920x1080+0 1920x1080+1280+0+0
     		hc pad 0 0 0 0 0
     		hc pad 1 20 0 20 0
@@ -21,4 +21,3 @@ for monitor in $(xrandr --listmonitors | grep -v Monitors | wc -l) ; do
 done
 
 hc detect_monitors
-
