c2d571e2a435be88aa4d78eaf8bc9b24d395e9c5
Ricardo Tun <me@ricardotun.mx>
2021-12-25 02:07:55 -0600

    update herbstluftwm config

--- herbstluftwm/monitors.sh
+++ herbstluftwm/monitors.sh
@@ -2,22 +2,31 @@
 
 hc unlock
 
+hc spawn polybar main
+
+hc detect_monitors
+
 for monitor in $(xrandr --listmonitors | grep -v Monitors | wc -l) ; do
     # start it on each monitor
     case $monitor in
-    	1)
-			hc set_monitors 1920x1080+1280+0+0
-    		hc pad 0 0 0 0 0
-    		;;
+        1)
+            hc set_monitors 1920x1080+1280+0+0
+            case "$XDG_SESSION_DESKTOP" in
+                xfce )
+                    hc pad 0 50 100 50 100
+                    ;;
+                herbstluftwm )
+                    hc pad 0 50 100 50 100
+                    ;;
+            esac
+            ;;
 
-    	2)
-			hc spawn xrandr --auto --output HDMI1 --mode 1920x1080 --left-of eDP1
-			hc set_monitors 1920x1080+0 1920x1080+1280+0+0
-    		hc pad 0 0 0 0 0
-    		hc pad 1 20 0 20 0
-    		;;
+        2)
+            hc spawn xrandr --auto --output HDMI1 --mode 1920x1080 --left-of eDP1
+            hc set_monitors 1920x1080+0 1920x1080+1280+0+0
+            hc pad 0 0 0 0 0
+            hc pad 1 20 0 20 0
+            ;;
     esac
 
 done
-
-hc detect_monitors
